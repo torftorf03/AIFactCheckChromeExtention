641bda660384c0fe6f457b92225965da7a70d505
torftorf <torftorf03@gmx.de>
2025-03-14 19:26:04 +0100

    added config page and some styling

--- Extention/popup.js
+++ Extention/popup.js
@@ -1,35 +1,21 @@
-const button = document.getElementById("save-btn")
-const p = document.getElementById("status")
-const toggleButton = document.getElementById("toggle-key");
-const apiKeyContainer = document.getElementById("apiKeyContainer");
-const input = document.getElementById("apiKeyInput");
-const saveButton = document.getElementById("saveApiKey");
+const button = document.getElementById("save-btn");
+const p = document.getElementById("status");
+
+let APIKey;
 
 // Lade den gespeicherten API-Key
 chrome.storage.local.get("apiKey", function (data) {
     if (data.apiKey) {
-        input.value = data.apiKey;
+        APIKey= data.apiKey;
     }else{
 	button.disabled = true;
     }
 });
 
-// API-Key speichern
-input.addEventListener("input", function () {
-    chrome.storage.local.set({ apiKey: input.value });
-    button.disabled = false;
+document.getElementById("settings-btn").addEventListener("click", () => {
+    window.location.href = '/settings.html';
 });
 
-// Ein-/Ausblenden der API-Key Eingabe
-toggleButton.addEventListener("click", function () {
-    if (apiKeyContainer.style.display === "none") {
-        apiKeyContainer.style.display = "block";
-        toggleButton.textContent = "API-Key Eingabe ausblenden";
-    } else {
-        apiKeyContainer.style.display = "none";
-        toggleButton.textContent = "API-Key Eingabe anzeigen";
-    }
-});
 
 
 button.onclick = async () => {
@@ -69,21 +55,16 @@ button.onclick = async () => {
 };
 
 
-
-
-
 async function queryOpenAI(prompt) {
-    const apiKey = input.value;
     const url = 'https://api.openai.com/v1/chat/completions';
     const systempromt2 = "You are a fact-checker who reviews chat messages or conversation histories to verify the accuracy of statements. If you find any false claims, you provide a well-reasoned and clear correction, explaining the mistake in a professional yet approachable manner. Your response should match the tone and complexity of the original messages without sounding condescending. Always reply in the same language as the original text."
-
 
 
     const response = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + apiKey
+            'Authorization': 'Bearer ' + APIKey
         },
         body: JSON.stringify({
             model: 'gpt-4o-mini',
